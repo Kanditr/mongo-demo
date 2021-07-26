57de6edf4e8b4a055fa301e96391b4963b7bba5e
Kanditr <gun.rodwong@gmail.com>
2021-07-26 16:45:32 +0700

    winston

--- middleware/error.js
+++ middleware/error.js
@@ -1,14 +1,7 @@
 const winston = require("winston");
 
 module.exports = function (err, req, res, next) {
-  winston.error(err.message, err);
-
-  // error
-  // warn
-  // info
-  // verbose
-  // debug
-  // silly
+  winston.log("error", err.message);
 
   res.status(500).send("Something failed.");
 };
